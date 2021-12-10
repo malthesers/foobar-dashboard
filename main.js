be88e1503244e40d333ec2a4147be33f591ba94c
Mai Jockwich <mett08r7@stud.kea.dk>
2021-12-10 17:50:43 +0100

    Section manager added + most functioanlity

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 "use strict";
-import "./styles/style.scss";
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
+//import "./styles/style.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   getData();
+
+  getTheme();
+  document.querySelector(".theme-switch").addEventListener("change", switchTheme);
 
   document.querySelectorAll("nav a").forEach((a) => {
     a.addEventListener("click", (e) => {
@@ -25,9 +30,12 @@ function getData() {
       showBartenders(data.bartenders);
       showQueue(data.queue);
       showServing(data.serving);
-      showTime(data.timestamp);
+      showTime(data.timestamp, data.bar.closingTime);
       getKegs(data.taps);
       showTotalOrders(data.queue);
+      showOrderNumbers(data.queue, data.serving);
+      showInventory(data.storage);
+      showSales(data.queue);
 
       //Recall functin after 1s
       setTimeout(getData, 1000);
@@ -36,8 +44,21 @@ function getData() {
 
 function showBartenders(bartenders) {
   bartenders.forEach((bartender) => {
-    const bartenderServing = document.querySelector(`article[data-bartender="${bartender.name}"] .bartender-serving`);
-    const bartenderStatus = document.querySelector(`article[data-bartender="${bartender.name}"] .bartender-status`);
+    const bartenderServing = document.querySelector(`#section-bartenders article[data-bartender="${bartender.name}"] .bartender-serving`);
+    const bartenderStatus = document.querySelector(`#section-bartenders article[data-bartender="${bartender.name}"] .bartender-status`);
+
+    if (bartender.servingCustomer === null) {
+      bartenderServing.textContent = "Not serving";
+    } else {
+      bartenderServing.textContent = `Serving: #${bartender.servingCustomer}`;
+    }
+
+    //bartenderStatus.textContent = `Status: ${bartender.status}`;
+  });
+  
+  bartenders.forEach((bartender) => {
+    const bartenderServing = document.querySelector(`#section-manager article[data-bartender="${bartender.name}"] .bartender-serving`);
+    const bartenderStatus = document.querySelector(`#section-manager article[data-bartender="${bartender.name}"] .bartender-status`);
 
     if (bartender.servingCustomer === null) {
       bartenderServing.textContent = "Not serving";
@@ -185,21 +206,39 @@ function showServing(orders) {
   });
 }
 
-function replaceBeer(beer) {
-  beer === "Ruined Childhood" ? (beer = "GitHop") : beer;
-  beer === "Steampunk" ? (beer = "El Hefe") : beer;
-  beer === "Row 26" ? (beer = "Sleighride") : beer;
+function showTime(timestamp, closingTime) {
+  const date = new Date(timestamp);
+  const hours = getTime(date, "hours");
+  const minutes = getTime(date, "minutes");
+  const seconds = getTime(date, "seconds");
 
-  return beer;
+  const time = `${hours}:${minutes}:${seconds}`
+
+  document.querySelector(".header-current-time").textContent = time;
+
+  showRemainingTime(time, closingTime);
 }
 
-function showTime(timestamp) {
-  const time = new Date(timestamp);
-  const hours = getTime(time, "hours");
-  const minutes = getTime(time, "minutes");
-  const seconds = getTime(time, "seconds");
+function showRemainingTime (currentTime, closingTime) {
+  //Split time into hours, min and sec
+  const currentTimes = currentTime.split(":");
+  const closingTimes = closingTime.split(":");
 
-  document.querySelector(".header-time").textContent = `${hours}:${minutes}:${seconds}`;
+  //Convert to dates
+  const currentDate = new Date(0, 0, 0, currentTimes[0], currentTimes[1], currentTimes[2])
+  const closingDate = new Date(0, 0, 0, closingTimes[0], closingTimes[1], closingTimes[2])
+
+  //Get difference in UNIX
+  const timeDiff = closingDate.getTime() - currentDate.getTime();
+
+  //Calculate hours, min and sec based on difference
+  const remainingHours = Math.floor(timeDiff / 1000 / 60 / 60).toString().padStart(2, "0");
+  const remainingMin = Math.floor((timeDiff / 1000 / 60) - (remainingHours * 60)).toString().padStart(2, "0");
+  const remainingSec = Math.floor((timeDiff / 1000) - (remainingHours * 60 * 60) - (remainingMin * 60)).toString().padStart(2, "0");
+  
+  const remainingTime = `${remainingHours}:${remainingMin}:${remainingSec}`;
+
+  document.querySelector(".header-remaining-time").textContent = `Closing in: ${remainingTime}`;
 }
 
 function getTime(time, unit) {
@@ -216,6 +255,269 @@ function showTotalOrders(queue) {
 
     const totalOrders = queue[index].id;
 
-    document.querySelector(".total-orders").textContent = totalOrders;
+    document.querySelector("#section-bartenders .total-orders").textContent = totalOrders;
   }
+
+  if (queue.length > 0) {
+    const index = queue.length - 1;
+
+    const totalOrders = queue[index].id;
+
+    document.querySelector("#section-manager .total-orders").textContent = totalOrders;
+  }
+
+
+}
+
+function getTheme () {
+  //Get locally stored theme
+  const theme = localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark";
+
+  //Change theme if not default/dark
+  if(theme === "light") {
+    document.querySelector("input").checked = true;
+    switchTheme();
+  }
+}
+
+function switchTheme () {
+  const html = document.querySelector("html");
+  const logo = document.querySelector(".foobar-logo");
+
+  //Change theme based on previous theme and update logo
+  if (html.dataset.theme === "dark") {
+    html.dataset.theme = "light";
+    logo.style.backgroundImage = "url(./images/icons/foobar-logo-light.svg)";
+  } else if (html.dataset.theme === "light") {
+    html.dataset.theme = "dark";
+    logo.style.backgroundImage = "url(./images/icons/foobar-logo-dark.svg)";
+  }
+
+  //Store new theme locally
+  localStorage.setItem("theme", html.dataset.theme)
+}
+
+
+function showOrderNumbers(queue, serving) {
+  
+    const queueIndex = queue.order;
+
+    const totalQueue = queue.length;
+
+    const servingIndex = serving.order;
+
+    const totalServing = serving.length;
+
+
+    document.querySelector(".in-queue-number").textContent = totalQueue;
+    document.querySelector(".being-served-number").textContent = totalServing;
+
+  
+}
+
+function showInventory(kegs) {
+  const container = document.querySelector(".inventory-articles");
+  container.innerHTML = " ";
+
+  //Filter through beers
+  const filteredKegs = kegs.filter((keg) => {
+    //Go through beers to include for each beer
+    return beersToInclude.some((inclBeer) => {
+      //Return true only for beers in beersToInclude
+      return inclBeer === keg.name ? true : false;
+    });
+  });
+  
+  //Empty canvas container
+  document.querySelector("#inventory-chart-container").innerHTML = " ";
+
+  //Create new canvas element
+  const inventoryChart = document.createElement("canvas");
+  inventoryChart.id = "inventory-chart";
+  inventoryChart.width = "auto";
+  inventoryChart.height = "auto";
+  document.querySelector("#inventory-chart-container").appendChild(inventoryChart);
+
+
+  const labels = ["El Hefe", "Fairy Tale Ale", "GitHop", "Hollaback Lager", "Hoppily Ever After", "Mowintime", "Sleighride"];
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'Inventory',
+      data: [filteredKegs[0].amount,
+      filteredKegs[1].amount,
+      filteredKegs[2].amount,
+      filteredKegs[3].amount,
+      filteredKegs[4].amount,
+      filteredKegs[5].amount,
+      filteredKegs[6].amount
+    ],
+      backgroundColor: [
+        '#FFD750'
+      ],
+      borderColor: [
+        '#FFD750'
+      ], 
+      borderWidth: 1
+    }]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      maintainAspectRatio: false,
+      animation: {
+          duration: 0
+      },
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+    }
+  };
+
+  let chart = new Chart(
+    document.querySelector("#inventory-chart"),
+    config
+  )
+} 
+
+const orderCounter = [
+  {
+    name: "El Hefe",
+    amount: 0,
+  },{
+    name: "Fairy Tale Ale",
+    amount: 0,
+  },{
+    name: "GitHop",
+    amount: 0,
+  },{
+    name: "Hollaback Lager",
+    amount: 0,
+  },{
+    name: "Hoppily Ever After",
+    amount: 0,
+  },{
+    name: "Mowintime",
+    amount: 0,
+  },{
+    name: "Sleighride",
+    amount: 0,
+  }
+]
+
+let latestOrder = 0;
+
+function showSales(queue) {
+  const container = document.querySelector(".sales-articles");
+  container.innerHTML = " ";
+
+  //Iterate through each order
+  queue.forEach(currentOrder => {
+    //Execute only if order is newer than the latest id
+    if (latestOrder < currentOrder.id) {
+      //Update id to most recent order
+      latestOrder = currentOrder.id;
+
+      //Iterate through array of sales
+      currentOrder.order.forEach(orderedBeer => {
+        const beer = replaceBeer(orderedBeer);
+
+        //Update sold amount of each beer
+        orderCounter.forEach(beerCounter => {
+          if (beer === beerCounter.name) {
+            beerCounter.amount++;
+          }
+        })
+      })
+    }
+  })
+
+  // orderCounter.forEach(beer=>{
+  //   const kegClone = document.querySelector("#template-sales").cloneNode(true).content;
+
+  //   kegClone.querySelector(".sales-name").textContent = beer.name;
+  //   kegClone.querySelector(".sales-amount").textContent = beer.amount;
+
+  //   container.appendChild(kegClone);
+  // })
+
+  //Empty canvas container
+  document.querySelector("#sales-chart-container").innerHTML = " ";
+
+  //Create new canvas element
+  const salesChart = document.createElement("canvas");
+  salesChart.id = "sales-chart";
+  salesChart.width = "auto";
+  salesChart.height = "auto";
+  document.querySelector("#sales-chart-container").appendChild(salesChart);
+
+
+  const labels = ["El Hefe", "Fairy Tale Ale", "GitHop", "Hollaback Lager", "Hoppily Ever After", "Mowintime", "Sleighride"];
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'Todays sale',
+      data:[
+        orderCounter[0].amount,
+        orderCounter[1].amount,
+        orderCounter[2].amount,
+        orderCounter[3].amount,
+        orderCounter[4].amount,
+        orderCounter[5].amount,
+        orderCounter[6].amount
+    ],
+      backgroundColor: [
+        '#70AF70'
+      ],
+      borderColor: [
+        '#70AF70'
+      ], 
+      borderWidth: 1
+    }]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      maintainAspectRatio: false,
+      animation: {
+          duration: 0
+      },
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+    }
+  };
+
+  let chart = new Chart(
+    document.querySelector("#sales-chart"),
+    config
+  )
+
+
+}
+
+function replaceBeer(beer) {
+  beer === "Ruined Childhood" ? (beer = "GitHop") : beer;
+  beer === "Steampunk" ? (beer = "El Hefe") : beer;
+  beer === "Row 26" ? (beer = "Sleighride") : beer;
+
+  return beer;
 }
